12592b8995c1ed3f0a35207e6c49e9db42e17e00
Diego <diegopaivaa15@gmail.com>
2025-09-03 19:17:41 -0300

    Css

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import './styles/theme.css';
+import './styles/Global.css';
+
 export function App(){
+    console.log("oi")
      
      return (
-     <div>
+     <> 
       <h1>Ola Mundo</h1>
     <p>
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt nesciunt sunt cupiditate? In voluptatem nesciunt fugiat, voluptatum eligendi nisi, sapiente placeat consequuntur ullam commodi mollitia natus incidunt a? Optio, ad.
     </p>
-     </div>
+     </>
      );
 }
 
