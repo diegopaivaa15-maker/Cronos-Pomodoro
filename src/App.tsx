701162fed51bd650058a32677926aa22d760dd1d
Diego <diegopaivaa15@gmail.com>
2025-09-03 19:46:42 -0300

    Escopo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
+import { Heading } from './componentes/Heading';
+
 import './styles/theme.css';
 import './styles/Global.css';
+
 
 export function App(){
     console.log("oi")
      
      return (
      <> 
-      <h1>Ola Mundo</h1>
+    <Heading/>
     <p>
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt nesciunt sunt cupiditate? In voluptatem nesciunt fugiat, voluptatum eligendi nisi, sapiente placeat consequuntur ullam commodi mollitia natus incidunt a? Optio, ad.
     </p>
---
+++ src/componentes/Heading.tsx
@@ -0,0 +1,5 @@
+import styles from './Home.module.css';
+
+export function Heading(){
+    return <h1 className={styles.heading}>Hello</h1>;
+ }
