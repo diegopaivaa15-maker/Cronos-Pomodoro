4f1801d3e3b835ff86e8bb551d430948b96f2a49
Diego <diegopaivaa15@gmail.com>
2025-09-04 19:14:27 -0300

    Começando Css

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,18 @@ import { Heading } from './componentes/Heading';
 
 import './styles/theme.css';
 import './styles/Global.css';
+import { TimerIcon } from 'lucide-react';
 
 
 export function App(){
-    console.log("oi")
-     
      return (
      <> 
-    <Heading/>
+     <Heading>
+      Ola Mundo
+      <button>
+        <TimerIcon/>
+      </button>
+     </Heading>
     <p>
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt nesciunt sunt cupiditate? In voluptatem nesciunt fugiat, voluptatum eligendi nisi, sapiente placeat consequuntur ullam commodi mollitia natus incidunt a? Optio, ad.
     </p>
--- src/componentes/Heading.tsx
+++ src/componentes/Heading.tsx
@@ -1,5 +1,12 @@
+import type React from 'react';
 import styles from './Home.module.css';
 
-export function Heading(){
-    return <h1 className={styles.heading}>Hello</h1>;
+type HeadingProps = {
+    children: React.ReactNode;
+};
+
+export function Heading(props: HeadingProps){
+    // puxar uma chave dentro do objeto 
+    const children = props.children;
+    return <h1 className={styles.heading}>{children}</h1>;
  }
