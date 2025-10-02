f194835fbe77c2365600cc3468775180db3ed519
Diego Paiva <diegopaivaa15@gmail.com>
2025-10-02 11:26:26 -0300

    Alertas

--- src/componentes/Cycles/indext.tsx
+++ src/componentes/Cycles/indext.tsx
@@ -1,18 +1,19 @@
 import styles from './styles.module.css';
 
 export function Cycles(){
-    return <div className={styles.cycles}>
+    return ( <div className={styles.cycles}>
         <span>Ciclos:</span>
 
         <div className={styles.cyclesDots}>
-            <span className={'${styles.cycleDot} ${styles.workTime}'}></span>
-            <span className={'${styles.cycleDot} ${styles.shortBreakTime}'}></span>
-            <span className={'${styles.cycleDot} ${styles.workTime}'}></span>
-            <span className={'${styles.cycleDot} ${styles.shortBreakTime}'}></span>
-            <span className={'${styles.cycleDot} ${styles.workTime}'}></span>
-            <span className={'${styles.cycleDot} ${styles.shortBreakTime}'}></span>
-            <span className={'${styles.cycleDot} ${styles.workTime}'}></span>
-            <span className={'${styles.cycleDot} ${styles.longBreakTime}'}></span>
+            <span className={`${styles.cycleDot} ${styles.workTime}`}></span>
+            <span className={`${styles.cycleDot} ${styles.shortBreakTime}`}></span>
+            <span className={`${styles.cycleDot} ${styles.workTime}`}></span>
+            <span className={`${styles.cycleDot} ${styles.shortBreakTime}`}></span>
+            <span className={`${styles.cycleDot} ${styles.workTime}`}></span>
+            <span className={`${styles.cycleDot} ${styles.shortBreakTime}`}></span>
+            <span className={`${styles.cycleDot} ${styles.workTime}`}></span>
+            <span className={`${styles.cycleDot} ${styles.longBreakTime}`}></span>
         </div>
     </div>
+    );
 }
