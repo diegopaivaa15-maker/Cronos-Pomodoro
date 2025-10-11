4e97394bf96d38ce85f93b971d8893ebc8ae3053
Diego Paiva <diegopaivaa15@gmail.com>
2025-10-11 16:55:46 -0300

    componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,12 @@
 import './styles/Theme.css'
 import './styles/Global.css'
-
-
-
-import { Container } from './componentes/Container';
-import { Logo } from './componentes/Logo';
-import { Menu } from './componentes/Menu';
-import { CountDown } from './componentes/CountDown';
-import { DefaultInput } from './componentes/DefaultInput';
-import { Cycles } from './componentes/Cycles/indext';
-import { DefaultButton } from './componentes/DefaultButton';
-import { PlayCircle } from 'lucide-react';
-import { Footer } from './componentes/Footer';
+import { Home } from 'lucide-react';
 
 
 export function App(){
-    return ( 
-    <>
-            <Container>
-            <Logo/>
-            </Container>
-            
-                <Container>
-                    <Menu/>
-                    </Container>
+    return  <Home/>;
+   
+     } 
+ 
 
-                        <Container>
-                            <CountDown/>
-                         </Container>
-
-                            <Container>
-                            <form className='form' action=''>
-                                <div className='formRow'>
-                                   <DefaultInput labelText='task' id="meuInpout" type='text' placeholder='Digite algo'/>
-                                </div>
-
-                                <div className='formRow'>
-                                    <p>Lorem ipsum dolor sit amet.</p>
-                                </div>
-
-                                <div className='formRow'>
-                                  <Cycles/>
-                                </div>
-
-                                <div className='formRow'>
-                                  <DefaultButton icon={< PlayCircle/> } color='red' />
-                                </div>
-                            </form>
-                         </Container>
-
-                         <Container>
-                          <Footer/>
-                         </Container>
-                         
-   </>
- );
-}
 
--- src/componentes/mainForm/index.tsx
+++ src/componentes/mainForm/index.tsx
@@ -6,21 +6,21 @@ import { DefaultInput } from "../DefaultInput";
 export function MainForm(){
     return(
          <form className='form' action=''>
-                                <div className='formRow'>
-                                   <DefaultInput labelText='task' id="meuInpout" type='text' placeholder='Digite algo'/>
-                                </div>
+             <div className='formRow'>
+             <DefaultInput labelText='task' id="meuInpout" type='text' placeholder='Digite algo'/>
+             </div>
 
-                                <div className='formRow'>
-                                    <p>Lorem ipsum dolor sit amet.</p>
-                                </div>
+                <div className='formRow'>
+                 <p>Lorem ipsum dolor sit amet.</p>
+                 </div>
 
-                                <div className='formRow'>
-                                  <Cycles/>
-                                </div>
+                <div className='formRow'>
+                <Cycles/>
+                </div>
 
-                                <div className='formRow'>
-                                  <DefaultButton icon={< PlayCircle/> } color='red' />
-                                </div>
-                            </form>
+                <div className='formRow'>
+                <DefaultButton icon={< PlayCircle/> } color='red' />
+                 </div>
+                </form>
     );
 }
